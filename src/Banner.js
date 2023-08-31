33427e186848ee06aab24849f1727be3635fe73f
estebanleandro <emarin@outcoding.com>
2023-08-31 14:40:37 -0600

    changed the content added the install button on chrome.

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,28 @@
-window.AppPrompt;
-
 window.addEventListener('beforeinstallprompt', (e) => {
-  console.log('installed!');
+  const btnAdd = document.getElementById('btnAdd');
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
-  AppPrompt = e;
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
 
-  console.log("The prompt is ready to install!", e);
+
+  btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+  
 });
