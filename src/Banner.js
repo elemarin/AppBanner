598880cda1cf650d10bda0505051a4acbbcac89c
estebanleandro <emarin@outcoding.com>
2023-09-05 14:32:56 -0600

    removing old features

--- src/Banner.js
+++ src/Banner.js
@@ -1,17 +1,17 @@
 const btnAdd = document.getElementById('btnAdd');
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  const text = document.querySelector('[data-text]');
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  btnAdd.style.display = 'block';  
-});
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   const text = document.querySelector('[data-text]');
+//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update UI notify the user they can add to home screen
+//   btnAdd.style.display = 'block';  
+// });
 
-btnAdd.addEventListener('click', (e) => {
-  // Show the prompt
-  deferredPrompt.prompt();
-});
+// btnAdd.addEventListener('click', (e) => {
+//   // Show the prompt
+//   deferredPrompt.prompt();
+// });
