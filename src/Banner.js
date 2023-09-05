def63d9caa4706fe2eb9a40620d7cabc28bea169
estebanleandro <emarin@outcoding.com>
2023-09-05 14:52:55 -0600

    removing the prevent default

--- src/Banner.js
+++ src/Banner.js
@@ -1,24 +1,15 @@
-// const btnAdd = document.getElementById('btnAdd');
-// let deferredPrompt;
-
-// window.addEventListener('beforeinstallprompt', (e) => {
-//   const text = document.querySelector('[data-text]');
-//   // Prevent Chrome 67 and earlier from automatically showing the prompt
-//   e.preventDefault();
-//   // Stash the event so it can be triggered later.
-//   deferredPrompt = e;
-//   // Update UI notify the user they can add to home screen
-//   btnAdd.style.display = 'block';  
-// });
-
-// btnAdd.addEventListener('click', (e) => {
-//   // Show the prompt
-//   deferredPrompt.prompt();
-// });
-
+const btnAdd = document.getElementById('btnAdd');
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
+  const text = document.querySelector('[data-text]');
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';  
+});
+
+btnAdd.addEventListener('click', (e) => {
+  // Show the prompt
+  deferredPrompt.prompt();
 });
